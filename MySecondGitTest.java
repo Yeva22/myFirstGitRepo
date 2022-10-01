3bfd9879db39bf62558bfe2bf279548d5267af0b
Yeva22 <y.shimanayev@gmail.com>
2022-10-01 14:46:05 -0400

    my branch addition

--- MySecondGitTest.java
+++ MySecondGitTest.java
@@ -2,5 +2,7 @@ public class MySecondGitTest {
     public static void main(String[] args) {
         System.out.println("My Test");
         System.out.println("My New Branch");
+        String day = "Saturday";
+
     }
 }
